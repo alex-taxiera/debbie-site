ffc8c8bf892a4426a1784cb31ed926344c5eb8a3
alex-taxiera <alex.taxiera@gmail.com>
2018-11-07 19:11:58 -0500

    add example pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function render () {
   return (
-    <div>about</div>
+    <div>
+      <p>
+        Debbie Chen was born and raised in Brockton Massachusetts.  A passionate music educator and Bassoonist, she studies music at the University of Massachusetts Amherst where she is earning her Bachelor of Music in Music education and Bassoon performance. Her former teachers include Remy Taghavi, Stephen Walt and Janet Underhill. She will receive her teaching license in music grade Pre-k - 12 in February 2019.
+      </p>
+      <p>
+        As a bassoonist, Ms. Chen as performed with the Umass wind ensemble, Umass symphony orchestra,  Umass symphony band, Umass all-university orchestra, UMMB, Umass Marimba ensemble, the Pioneer Valley symphony orchestra, Windham orchestra, Mt. Holyoke college Orchestra, Boston youth symphony orchestra, Sharon community band and Hanson. She has also performed solo recitals her junior and senior year of college.
+      </p>
+      <p>
+        My hopes and dreams  is to provide a high level of music and music education to schools, communities and anywhere else music can reach.
+      </p>
+      <p>
+        Ms. Chen completed her student teaching at Pembroke High school where she taught and worked with Freshmen band, Concert Band, Wind ensemble, Concert Choir, Guitar lab I, and piano lab I. She is currently student teaching in Dedham at Riverdale elementary and the Early childhood education center, where she is working with kindergarteners and 5th grade utilizing the music learning theory and the Gordon approach.
+      </p>
+    </div>
   )
 }
