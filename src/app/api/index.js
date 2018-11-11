63ece3d289aafdb9dd758022ba8b4f9a7ae1305f
alex-taxiera <alex.taxiera@gmail.com>
2018-11-10 20:08:00 -0500

    clean up the test stuff

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -5,52 +5,6 @@ const HEADERS = {
 }
 
 export async function getCaptchaKey () {
-  function getUserIP (onNewIP) {
-    var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection
-    const pc = new PeerConnection({
-      iceServers: []
-    })
-    const noop = () => {}
-    const localIPs = {}
-    const ipRegex = /\b((?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(?:(?<!\.)\b|\.)){4}/g
-
-    function iterateIP (ip) {
-      if (!localIPs[ip]) onNewIP(ip)
-      localIPs[ip] = true
-    }
-
-    pc.createDataChannel('')
-
-    // create offer and set local description
-    pc.createOffer(function (sdp) {
-      sdp.sdp.split('\n').forEach(function (line) {
-        if (line.indexOf('candidate') < 0) return
-        line.match(ipRegex).forEach(iterateIP)
-      })
-
-      pc.setLocalDescription(sdp, noop, noop)
-    }, noop)
-
-    pc.onicecandidate = function (ice) {
-      if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return
-      ice.candidate.candidate.match(ipRegex).forEach(iterateIP)
-    }
-  }
-
-  getUserIP(function (ip) {
-    console.log('ip', ip)
-    return fetch('http://' + ip + ':3001' + '/recaptcha', {
-      headers: HEADERS
-    }).then((res) => res.json())
-      .then((res) => {
-        if (res.error) {
-          console.error('test error', res.error)
-        } else {
-          console.log('test key', res.sitekey)
-        }
-      })
-  })
-
   return fetch(BASE_URL + '/recaptcha', {
     headers: HEADERS
   }).then((res) => res.json())
@@ -61,6 +15,7 @@ export async function getCaptchaKey () {
         return res.sitekey
       }
     })
+    .catch(console.error)
 }
 
 export async function sendContactEmails ({ to, from, text, html }) {
