e33344750d36a2bc9a57a4e15831e8e278a1c748
alex-taxiera <alex.taxiera@gmail.com>
2018-12-04 21:21:11 -0500

    add sample work (wip)

--- src/pages/sample/index.js
+++ src/pages/sample/index.js
@@ -1,19 +1,74 @@
 import React, { Component } from 'react'
 
+import YoutubeIframe from '../../app/components/youtube-iframe'
+import Spinner from '../../app/components/spinner'
+
+import './index.css'
+
+const SPINNER_COLOR = 'rgba(100,100,100,0.2)'
+
 class Sample extends Component {
   render = () => {
     return (
       <div>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget pretium felis, mollis consequat dui. Nam convallis ex eros. Cras scelerisque sem ante, quis pharetra mi auctor a. Donec varius accumsan ante, ac luctus ligula ornare efficitur. Aliquam vulputate, orci pellentesque consectetur pulvinar, diam ex volutpat ipsum, ac elementum tortor erat vitae libero. Quisque libero ex, tempor ac arcu ac, molestie tincidunt enim. Donec vel vehicula arcu, dapibus aliquam purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam sagittis magna eu ex porta pretium. Suspendisse potenti. Sed ullamcorper vel felis ullamcorper fermentum. Aliquam metus tellus, cursus id tortor et, condimentum pellentesque diam. Praesent at consectetur elit. Mauris quis urna cursus, feugiat sem vitae, rhoncus nulla.
-
-          Vivamus blandit nisi orci, sit amet vulputate ante ullamcorper hendrerit. Aenean dictum egestas est, sed ornare massa tincidunt a. Donec convallis, augue non volutpat aliquam, libero eros efficitur erat, ut condimentum lorem massa quis metus. Sed pharetra nunc id luctus efficitur. Maecenas in efficitur sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi dolor orci, volutpat sed orci sit amet, aliquet gravida tortor. Proin rhoncus, risus non laoreet dignissim, risus ligula posuere augue, gravida feugiat massa lectus et lorem. Proin in elementum dui. Curabitur tempus eros non mi tempus, vestibulum vehicula neque finibus.
-
-          Morbi metus sapien, rutrum non pharetra vel, varius eget arcu. Ut massa lacus, dictum ut nulla rhoncus, malesuada posuere nisi. Fusce ac dui velit. Proin accumsan pellentesque malesuada. Fusce at rutrum leo. Praesent commodo dui eros, vitae tempus mauris porta condimentum. In augue orci, venenatis vel varius ut, maximus id libero. Donec cursus scelerisque mi id convallis. Quisque congue, ligula vel bibendum accumsan, sapien nunc faucibus purus, non eleifend sapien nisl eu justo. Etiam quis ipsum nisl.
-
-          Curabitur neque diam, pulvinar vitae congue in, eleifend eu eros. Phasellus venenatis diam sit amet egestas iaculis. Nunc sagittis ligula sodales lacus finibus, eu sagittis purus vestibulum. Morbi quis molestie odio. Ut tincidunt, risus vel iaculis cursus, lorem augue auctor tortor, consequat tincidunt sapien tellus rhoncus nibh. Donec ac dolor porta, gravida elit sed, ultrices enim. Morbi placerat, quam vel iaculis sagittis, justo velit accumsan turpis, interdum dictum turpis nunc id ligula. Vivamus tincidunt aliquet euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-
-          Fusce interdum sollicitudin mi eu posuere. Nam quis hendrerit est, vitae aliquam eros. Maecenas in tempus velit. Nunc quis nisi arcu. Praesent ultrices molestie nisi. Sed in rhoncus turpis. In quis neque vitae risus tempor elementum. Mauris in felis sed est luctus tristique. Ut venenatis arcu sed pretium suscipit. Nam congue diam sit amet tellus faucibus euismod. Proin semper erat auctor lectus rutrum, tempor mollis justo mattis. Quisque id libero erat. Aenean odio erat, interdum sed feugiat vitae, facilisis quis ante.
+        <div className='schools'>
+          <div className='pembroke'>
+            <div className='description'>
+              <div className='float-left text-right wide-break' style={{ textAlign: 'center' }}>
+                <img src='/pembroke.png' alt='pembroke.png' style={{ maxWidth: 600 }} />
+              </div>
+              <h3>Pembroke High School</h3>
+              <p>
+                Austin Glass, Band Director
+                <br />
+                Gwynne Chapman, Chorus Director
+                <br />
+                Greg Tarbox, Orchestra Director
+              </p>
+              <p className='clear-both'>
+                I completed my secondary placement at Pembroke High School in Pembroke, Massachusetts with Mr. Glass as my supervising practitioner. I was also very fortunate to have the opportunity to work with and learn from both Mrs. Chapman and Mr. Tarbox, the Choral and orchestral directors at the school. At this placement I worked with the Freshmen band, Concert band, Wind ensemble, and string orchestra. I observed and taught bits and pieces of a Guitar lab, as well as concert choir, and took over a Piano lab where I taught class to beginning pianists. After school I would work with students privately on their instruments when requested and would assist in after school groups such as men’s choir, women’s choir, Jazz band and rehearsals for the school musical.
+              </p>
+            </div>
+          </div>
+          <div className='riverdale-curran'>
+            <div className='description'>
+              <div className='float-left text-right wide-break'>
+                <img src='/riverdale.png' alt='riverdale.png' style={{ maxWidth: '50%' }} />
+                <img src='/curran.png' alt='curran.png' style={{ maxWidth: '50%' }} />
+              </div>
+              <h3>Curran Early Childhood Education Center and Riverdale Elementary</h3>
+              <p>
+                Heather Kirby, Music Educator
+              </p>
+              <p className='clear-both'>
+                I completed my elementary placement at the Early Childhood Education Center, and Riverdale Elementary school under the supervision of Mrs. Kirby in Dedham, Massachusetts. During my time in this placement I worked with preschoolers and kindergarteners at the ECEC and grades 1-5 at Riverdale where I utilized the music learning theory and the Gordon approach. I also directed a piece in both the 5th grade and 4th grade chorus. I created and taught a unit plan on mexican music where the 4th graders learned a few songs in the mexican language and a game from Ecuador that was also in spanish. They performed it in their winter concert.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='videos clear-both'>
+          <h2>Performances</h2>
+          <div className='video padded-half'>
+            <YoutubeIframe
+              title='jesus is coming'
+              src='https://www.youtube.com/embed/9G-OTPH6si0'
+              preloader={(<Spinner color={SPINNER_COLOR} />)}
+            />
+          </div>
+          <div className='video padded-half'>
+            <YoutubeIframe
+              title='jesus is coming'
+              src='https://www.youtube.com/embed/kQ1tPMaFCKI'
+              preloader={(<Spinner color={SPINNER_COLOR} />)}
+            />
+          </div>
+          <div className='video'>
+            <YoutubeIframe
+              title='jesus is coming'
+              src='https://www.youtube.com/embed/oaIpk6yqQmM'
+              preloader={(<Spinner color={SPINNER_COLOR} />)}
+            />
+          </div>
         </div>
       </div>
     )
