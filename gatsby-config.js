c9e518b6b7728b5068a855d08cbde8ed09b7d91a
alex-taxiera <alex.taxiera@gmail.com>
2019-02-11 00:18:49 -0500

    use favicon correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relativeto the root of the site .
+        icon: `src/images/favicon.png` // This path is relativeto the root of the site .
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
