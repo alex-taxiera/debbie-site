6b852ef93dde17046659d43f254483d6dbf1593d
alex-taxiera <alex.taxiera@gmail.com>
2019-02-11 00:20:19 -0500

    make a nice 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../containers/layout"
+import Layout from '../containers/layout'
 
 const NotFoundPage = () => (
-  <Layout title="404: Not found">
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout seo={{
+    title: '404: Not found'
+  }}>
+    <div
+      style={{
+        textAlign: `center`
+      }}
+    >
+      <h1 style={{
+        fontSize: `20vw`,
+        color: `#825d4e`,
+        background: `-webkit-linear-gradient(#825d4e, #E7CFB4)`,
+        WebkitBackgroundClip: `text`,
+        WebkitTextFillColor: `transparent`
+      }}>404</h1>
+      <p>The page you were looking for doesn't exist.</p>
+      <p>Let's get you <Link
+        className="plain-link"
+        style={{ textDecoration: `underline` }}
+        to="/"
+      >home.</Link></p>
+    </div>
   </Layout>
 )
 
