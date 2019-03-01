cc3a4a6b3901810bd194bd5e0ac532d2e66e6c8d
alex-taxiera <alex.taxiera@gmail.com>
2019-03-01 10:33:14 -0500

    add carousel to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import Layout from 'containers/layout'
+import withScreenWidth from 'enhancers/with-screen-width'
 import Inspo from 'icons/inspo.svg'
 import 'graphql/images'
+import 'graphql/aws'
+import { Carousel } from 'react-responsive-carousel'
 
-const IndexPage = () => (
+const IndexPage = ({ screenWidth }) => (
   <StaticQuery
     query={graphql`
       query {
         debbiePlaying: file(relativePath: { eq: "debbie-playing.png" }) {
           ...fluid1024
+        }
+        s3Large: allS3Image {
+          ...fixed373
+        }
+        s3Small: allS3Image {
+          ...fixed200
         }
       }
     `}
@@ -23,46 +33,27 @@ const IndexPage = () => (
         <div id="fancy-text" className="padded">
           <Inspo />
         </div>
-        <Img fluid={data.debbiePlaying.childImageSharp.fluid} />
-        {/* {!images
-          ? (
-            <div className='carousel-placeholder'>
-              <Spinner color='rgba(100,100,100,0.2)' />
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          showIndicators={false}
+          infiniteLoop
+          autoPlay
+          interval={7000}
+        >
+          {(screenWidth > 480 ? data.s3Large : data.s3Small).images.map(({ node: { file } }, key) => (
+            <div className="carousel-slide-wrapper" key={key} style={{ height: (screenWidth > 480 ? 373 : 200) }}>
+              <Img fixed={file.data.src} />
             </div>
-          ) : (
-            <Carousel
-              autoGenerateStyleTag={false}
-              transitionMode='fade'
-              autoplay
-              autoplayInterval={10 * 1000}
-              pauseOnHover
-              swiping
-              wrapAround
-              renderBottomCenterControls={() => null}
-            >
-              {
-                images.map((name, index) => {
-                  return (
-                    <img
-                      key={index}
-                      src={`${IMAGE_URL}/${name}`}
-                      alt=''
-                      style={{ display: 'block', margin: 'auto' }}
-                      onLoad={index > 0 ? () => null : () => window.dispatchEvent(new Event('resize'))}
-                    />
-                  )
-                })
-              }
-            </Carousel>
-          )} */
-        }
+          ))}
+        </Carousel>
         <div className="padded">
           <p className="with-top" style={{ textAlign: 'center', marginBottom: 0 }}>
             I believe that music should be enjoyed by everyone and anyone.
             <br />
             I want to help students learn and understand music so they can find ways they can enjoy music.
             <br />
-            I believe that  music should be fun and enjoyable for those who partake in music.
+            I believe that music should be fun and enjoyable for those who partake in music.
             <br />
             As a musician I want to touch the heart and stir the emotions of those who listen to my performance.
           </p>
@@ -72,10 +63,14 @@ const IndexPage = () => (
   />
 )
 
+IndexPage.propTypes = {
+  screenWidth: PropTypes.number.isRequired
+}
+
 export const frontmatter = {
   title: 'Home',
   path: '/',
   navPosition: 0
 }
 
-export default IndexPage
+export default withScreenWidth(IndexPage)
