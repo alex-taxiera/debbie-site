7ee7a529e7513b54d0bec2757a0ab36fa81e6ffc
alex-taxiera <alex.taxiera@gmail.com>
2019-03-01 10:26:49 -0500

    supress issue from superagent

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,17 @@
 const path = require('path')
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({
+  plugins,
+  actions
+}) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    }
+    },
+    plugins: [
+      plugins.define({
+        'global.GENTLY': false
+      })
+    ]
   })
 }
