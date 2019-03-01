420054920fdca1f1774a2b047344fba98712ac05
alex-taxiera <alex.taxiera@gmail.com>
2019-03-01 10:34:36 -0500

    add recaptcha and carousel style

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
+import './node_modules/react-responsive-carousel/lib/styles/carousel.min.css';
+
 import 'styles/global.scss'
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <GoogleReCaptchaProvider reCaptchaKey={process.env.CAPTCHA_KEY}>
+      {element}
+    </GoogleReCaptchaProvider>
+  )
+}
